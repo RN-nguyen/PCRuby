bc17b9fad2811383995a4553de128e45147f75fc
onguyen89@gmail.com <rebeccanguyen@Rebeccas-MacBook-Pro.local>
2014-01-20 00:04:36 -0700

    initial commit

---
+++ personal_chef.rb
@@ -0,0 +1,21 @@
+class PersonalChef
+  def make_toast (color)
+    puts "Making your #{color} oast !"
+  return self
+end
+
+  def make_eggs (quantity)
+    puts "Making you #{ quantity} eggs!"
+    return self
+  end
+
+  def make_milkshake (taste)
+    puts "You deserve a great #{taste} milkshake!"
+    return self
+  end
+
+class Butler
+  def open_front_door
+    puts "Opening your door!"
+  return self
+end
